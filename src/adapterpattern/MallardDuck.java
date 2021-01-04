7d7ad28dd67b46fe9218957c9485f03c4a1fded0
Jung Insup <jisjis1218@gmail.com>
2021-01-04 21:56:32 +0900

    adapter pattern

---
+++ src/adapterpattern/MallardDuck.java
@@ -0,0 +1,17 @@
+package adapterpattern;
+
+public class MallardDuck implements Duck {
+
+	@Override
+	public void quack() {
+		// TODO Auto-generated method stub
+		System.out.println("Quack");
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		System.out.println("duck flying");
+	}
+
+}
